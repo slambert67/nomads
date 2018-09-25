6866d5a3a15b8f300bfcc5312cf2a6427f2c414f
Steve Lambert <steve.lambert67@gmail.com>
2018-09-25 21:09:57 +0100

    wip

---
+++ firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDxgEiXAJEvXAA4CDsF1yXlQaIczU3skgo",
+    authDomain: "nomads-d85b5.firebaseapp.com",
+    databaseURL: "https://nomads-d85b5.firebaseio.com",
+    projectId: "nomads-d85b5",
+    storageBucket: "",
+    messagingSenderId: "608271983758"
+  };
+  firebase.initializeApp(config);
