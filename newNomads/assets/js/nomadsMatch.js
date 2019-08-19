b469e8d1bf0aae5fafbe097d8a534f83669b10dd
Steve Lambert <steve.lambert67@gmail.com>
2019-08-19 20:49:36 +0100

    wip

--- newNomads/assets/js/nomadsMatch.js
+++ newNomads/assets/js/nomadsMatch.js
@@ -12,12 +12,33 @@ var match = (function () {
 
     function updatePlayerStats(t1p1DocId, t1p2DocId, t2p1DocId, t2p2DocId) {
 
-        var member = db.getMember(t1p1DocId);
+        /*var member = db.getMember(t1p1DocId);
         var memberData = member.data();
         var newRating = memberData.ladder.med.currentRating + 50;
         memberData.ladder.med.currentRating = newRating;
         db.updateMember(t1p1DocId, memberData);
-        console.log("hello");
+        console.log("hello");*/
+
+        // winner
+        var t1p1 = db.getMember(t1p1DocId);
+        var t1p1Data = t1p1.data();
+        var t1p1OldRating = t1p1Data.ladder.med.currentRating;
+        var t1p1TransformedRating = Math.pow(10,(t1p1OldRating / 400));
+
+        // loser
+        var t2p1 = db.getMember(t2p1DocId);
+        var t2p1Data = t2p1.data();
+        var t2p1OldRating = t2p1Data.ladder.med.currentRating;
+        var t2p1TransformedRating = Math.pow(10, (t2p1OldRating / 400));
+
+        var t1p1ExpectedScore = t1p1TransformedRating / (t1p1TransformedRating + t2p1TransformedRating);
+        var t2p1ExpectedScore = t2p1TransformedRating / (t2p1TransformedRating + t1p1TransformedRating); 
+
+        var t1p1NewRating = t1p1OldRating + (32 * (1 - t1p1ExpectedScore));
+        var t2p1NewRating = t2p1OldRating + (32 * (0 - t2p1ExpectedScore)); 
+
+        console.log("t1p1 new rating = " + t1p1NewRating);
+        console.log("t2p1 new rating = " + t2p1NewRating); 
     }
 
     return {
